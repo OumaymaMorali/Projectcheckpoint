39ee5ea50e02433d9c704d74184ee25314d29ea6
OumaymaMorali <71546206+OumaymaMorali@users.noreply.github.com>
2020-11-22 08:23:37 +0100

    Add files via upload

---
+++ Script/Script.js
@@ -0,0 +1,86 @@
+function myalert(){
+    Swal.fire({title:'Nothing found!',
+    confirmButton: true
+    
+})
+       
+}
+
+function psychalert(){
+    Swal.fire({title:'Your mind is an open book',
+
+})
+}
+function Submitmessage(){
+    Swal.fire({title:'Message Submitted',
+})
+}
+
+function subscribealert(){
+    Swal.fire({title:'Subscription successful!',
+})
+}
+
+
+function changeImage() {
+
+    if (document.getElementById("bi").src == "file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/Bipolar-disease-blue-white-silhouettes-on-blue-background.jpg") 
+    {
+        document.getElementById("bi").src = "file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/Bipolar-disease-blue-white-silhouettes-on-blue-background.jpg";
+    }
+    else 
+    {
+        document.getElementById("bi").src = "file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/Bipolar-disease-blue-white-silhouettes-on-blue-background.jpg";
+    }}
+
+
+    function change() {
+
+        if (document.getElementById("anti").src == "file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/anti.jpg") 
+        {
+            document.getElementById("anti").src = "file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/anti.jpg";
+        }
+        else 
+        {
+            document.getElementById("anti").src = "file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/anti.jpg";
+        }}
+
+        function transform(){
+            if(document.getElementById("dis").src=="file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/Multiple.jpg")
+            { document.getElementById("dis").src="file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/Multiple.jpg";
+        }
+
+        else{
+            document.getElementById("dis").src="file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/Multiple.jpg";
+        }
+
+
+
+        }
+
+        function trans(){
+            if(document.getElementById("schiz").src=="file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/Schizo2.jpg")
+            {
+                document.getElementById("schiz").src="file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/Schizo2.jpg";
+            }
+            else{
+                document.getElementById("schiz").src="file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/Schizo2.jpg";
+            }
+        }
+    function thanks(){
+        Swal.fire({title:'Thank you for your input!'})
+    }
+
+    function post(){
+        if (document.getElementById("post").src == "file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/ptsd2.jpg") 
+        {
+            document.getElementById("post").src="file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/ptsd2.jpg";
+        }
+        else{
+            document.getElementById("post").src="file:///C:/Users/Omaima/Desktop/Psychology%20Website/Img/ptsd2.jpg"
+        }
+	
+    }
+    
+
+    
